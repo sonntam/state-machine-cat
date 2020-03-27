396bb43ae544635c477c30b0324ebea9a7fb6fbd
Marcus Sonntag <sonntag.marcus@yahoo.com>
2020-03-27 10:59:39 +0100

    Fixed separation line in parallel compound states

--- src/render/svg.js
+++ src/render/svg.js
@@ -8,37 +8,106 @@ const { JSDOM } = jsdom;
 
 const viz = typeof viz_lib === "function" ? viz_lib : Viz;
 
-function adjustClusters(pClusterElement) {
-  let separatorLine = pClusterElement.querySelector("polygon");
-  let outerPath = pClusterElement.querySelector("path");
-
-  // Sanity check
-  if (!separatorLine || !outerPath) return;
-
-  // Get width (min/max x coordinates) of cluster from the path
-  let xCoords = outerPath
-    .getAttribute("d")
+function getBBoxFromCoordinates(pCoords) {
+  let coordPairs = pCoords
     .split(/[\smlhvcsqtaz]+/i)
     .filter(pE => pE)
-    .map(pE => parseFloat(pE.split(",")[0]));
+    .map(pE => pE.split(",").map(pEi => parseFloat(pEi)));
 
-  let xCoordMin = Math.min(...xCoords);
-  let xCoordMax = Math.max(...xCoords);
+  let xCoords = coordPairs.map(pE => pE[0]);
+  let yCoords = coordPairs.map(pE => pE[1]);
 
-  // Get y coordinate
-  let yCoord = parseFloat(
-    separatorLine
-      .getAttribute("points")
-      .trim()
-      .split(" ", 1)[0]
-      .split(",")[1]
+  return {
+    left: Math.min(...xCoords),
+    right: Math.max(...xCoords),
+    top: Math.min(...yCoords),
+    bottom: Math.max(...yCoords)
+  };
+}
+
+function getClusterParts(pClusterElement) {
+  let elements = {
+    separatorLine: null,
+    outerPath: null,
+    clusterType: "default"
+  };
+
+  elements.separatorLine = pClusterElement.querySelector(
+    "polygon:not([stroke-dasharray])"
+  );
+  elements.outerPath = pClusterElement.querySelector("path");
+
+  // Check if this may be a parallel cluster state
+  if (!elements.outerPath) {
+    elements.outerPath = pClusterElement.querySelector(
+      "polygon[stroke-dasharray]"
+    );
+    elements.clusterType = "parallel";
+  }
+
+  // Sanity check
+  if (!elements.separatorLine || !elements.outerPath) return null;
+
+  return elements;
+}
+
+function adjustParallelCluster(pClusterParts) {
+  const parent = pClusterParts.outerPath.parentNode;
+  let bboxOuter = getBBoxFromCoordinates(
+    pClusterParts.outerPath.getAttribute("points")
+  );
+  let bboxSeparator = getBBoxFromCoordinates(
+    pClusterParts.separatorLine.getAttribute("points")
   );
 
-  // Set new coordinates
-  separatorLine.setAttribute(
+  let lineElement = parent.ownerDocument.createElement("line");
+
+  lineElement.setAttribute("stroke-dasharray", "5,2");
+  lineElement.setAttribute(
+    "stroke",
+    pClusterParts.separatorLine.getAttribute("stroke")
+  );
+  lineElement.setAttribute("x1", bboxOuter.left);
+  lineElement.setAttribute("y1", bboxSeparator.top);
+  lineElement.setAttribute("x2", bboxOuter.right);
+  lineElement.setAttribute("y2", bboxSeparator.top);
+
+  parent.removeChild(pClusterParts.separatorLine);
+  parent.append(lineElement);
+}
+
+function adjustDefaultCluster(pClusterParts) {
+  let bboxOuter = getBBoxFromCoordinates(
+    pClusterParts.outerPath.getAttribute("d")
+  );
+  let bboxSeparator = getBBoxFromCoordinates(
+    pClusterParts.separatorLine.getAttribute("points")
+  );
+
+  pClusterParts.separatorLine.setAttribute(
     "points",
-    `${xCoordMin},${yCoord} ${xCoordMax},${yCoord}`
+    `${bboxOuter.left},${bboxSeparator.top} ${bboxOuter.right},${bboxSeparator.top}`
   );
+}
+
+function adjustCluster(pClusterElement) {
+  // Get relevant elements of cluster
+  let clusterParts = getClusterParts(pClusterElement);
+
+  // Sanity check
+  if (!clusterParts) return;
+
+  // Adjust according to type
+  switch (clusterParts.clusterType) {
+    case "parallel":
+      adjustParallelCluster(clusterParts);
+      break;
+    case "default":
+      adjustDefaultCluster(clusterParts);
+      break;
+    default:
+      return;
+  }
 }
 
 module.exports = (pAST, pOptions) => {
@@ -53,12 +122,12 @@ module.exports = (pAST, pOptions) => {
   // Get all cluster elements with a state description
   let clusters = Reflect.apply(
     Array.prototype.map,
-    document.querySelectorAll("svg g.cluster polygon"),
+    document.querySelectorAll("svg g.cluster polygon:not([stroke-dasharray])"),
     [pElement => pElement.parentNode]
   );
 
   // Adjust clusters
-  clusters.forEach(pE => adjustClusters(pE));
+  clusters.forEach(pE => adjustCluster(pE));
 
   return dom.serialize();
 };
